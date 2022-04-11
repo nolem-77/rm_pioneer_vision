96baaf746cfcb38f200acf4d6b81a2c18e671cee
chenjun <chen.junn@outlook.com>
2022-04-11 14:21:38 +0000

    Update submodule & add a useful cmd to script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Uncomment the following line if you want the container to do nothing on startup
+# tail -f /dev/null
+
 if [ ! "$(ls -A /root/ros_ws/src/rm_pioneer_config)" ]; then
   git clone https://github.com/chenjunnn/rm_pioneer_config -b ${ROBOT} src/rm_pioneer_config
 fi
