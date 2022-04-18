fd24af0252c73d9597f12f0d1ae2ef80cc890b67
nolem-77 <82574024+nolem-77@users.noreply.github.com>
2022-04-18 10:49:17 +0800

    Seperate camera package (#2)
    * Seperate camera package
    
    * Fix a bug
    
    * Fix some bug
    
    * Optimize for future update
    
    * Avoid compiling camera packet
    every time the container start
    
    * Remove realsense param
    
    * Fix a bug
    
    * Fix a bug

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -10,18 +10,23 @@ fi
 case $ROBOT in
     hero)
         export ROS_DOMAIN_ID=1
+        camera_type=mindvision
         ;;
     standard3)
         export ROS_DOMAIN_ID=3
+        camera_type=mindvision
         ;;
     standard4)
         export ROS_DOMAIN_ID=4
+        camera_type=hik
         ;;
     standard5)
         export ROS_DOMAIN_ID=5
+        camera_type=mindvision
         ;;
     guard)
         export ROS_DOMAIN_ID=6
+        camera_type=mindvision
         ;;
     *)
         echo "Unknown robot type: $ROBOT"
@@ -29,6 +34,29 @@ case $ROBOT in
 esac
 
 source /opt/ros/galactic/setup.zsh
+
+case $camera_type in
+        mindvision)
+            if [ ! "$(ls -A /root/ros_ws/src/ros2_mindvision_camera)" ]; then
+                git clone https://github.com/chenjunnn/ros2_mindvision_camera.git src/ros2_mindvision_camera
+                colcon build --symlink-install --packages-select ros2_${camera_type}_camera
+            fi
+            ;;
+        hik)
+            if [ ! "$(ls -A /root/ros_ws/src/ros2_hik_camera)" ]; then
+                git clone https://github.com/nolem-77/ros2_hik_camera.git src/ros2_hik_camera
+                colcon build --symlink-install --packages-select ros2_${camera_type}_camera
+
+                export MVCAM_SDK_PATH=/root/ros_ws/src/ros2_hik_camera/hikSDK
+                export MVCAM_COMMON_RUNENV=/root/ros_ws/src/ros2_hik_camera/hikSDK/lib
+                export LD_LIBRARY_PATH=/root/ros_ws/src/ros2_hik_camera/hikSDK/lib:$LD_LIBRARY_PATH
+            fi
+            ;;
+        *)
+            echo "Unknown camera type: $camera_type"
+            ;;
+esac
+
 colcon build --symlink-install --packages-select rm_pioneer_description rm_pioneer_bringup
 
 exec "$@"
